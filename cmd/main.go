687087e8d6f06030ec478b058561ace9f4f0e516
Sérgio Fagundes <contato.sergiofagundes@gmail.com>
2024-08-16 12:11:13 -0300

    Search Product By ID route

--- cmd/main.go
+++ cmd/main.go
@@ -36,6 +36,7 @@ func main() {
 
 	server.GET("/products", ProductController.GetProducts)
 	server.POST("/product", ProductController.CreateProduct)
+	server.GET("/product/:productId", ProductController.GetProductById)
 
 	server.Run(":8000")
 }
